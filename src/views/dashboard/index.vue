6bf92086b8147eaaefb5036533774c8bfaf54394
muzili <muzili@nb-chat.com>
2020-06-22 20:49:23 +0800

    修改默认首页

--- src/views/dashboard/index.vue
+++ src/views/dashboard/index.vue
@@ -23,7 +23,7 @@ export default {
     ])
   },
   created() {
-    if (!this.roles.includes('admin')) {
+    if (!this.roles.includes('dashboard')) {
       this.currentRole = 'editorDashboard'
     }
   }
