c8e508afdb9fc646265a698d225edb5c6f90be9b
划船不用桨 <jrseemail@gmail.com>
2020-12-07 13:12:34 +0800

    添加设置按钮y轴拖拽

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -23,10 +23,10 @@
         </div>
         <el-dropdown-menu slot="dropdown">
           <router-link to="/">
-            <el-dropdown-item>Dashboard</el-dropdown-item>
+            <el-dropdown-item>首页</el-dropdown-item>
           </router-link>
           <el-dropdown-item divided @click.native="logout">
-            <span style="display:block;">Log Out</span>
+            <span style="display:block;">注销登录</span>
           </el-dropdown-item>
         </el-dropdown-menu>
       </el-dropdown>
