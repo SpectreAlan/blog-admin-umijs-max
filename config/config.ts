8c40e223cf36a37eb69918e7953101c12f289deb
SpectreAlan <comjszoo@gmail.com>
2024-01-19 12:30:04 +0800

    add global config

---
+++ config/config.ts
@@ -0,0 +1,45 @@
+import { defineConfig } from '@umijs/max';
+
+export default defineConfig({
+    antd: {},
+    access: {},
+    model: {},
+    initialState: {},
+    request: {},
+    layout: {
+        title: 'blog admin',
+    },
+    routes: [
+        {
+            path: '/',
+            redirect: '/home',
+        },
+        {
+            name: '首页',
+            path: '/home',
+            component: './Home',
+        },
+        {
+            name: '权限演示',
+            path: '/access',
+            component: './Access',
+        },
+        {
+            name: ' CRUD 示例',
+            path: '/table',
+            component: './Table',
+        },
+    ],
+    npmClient: 'pnpm',
+    proxy: {
+        '/api': {
+            'target': process.env.PROXY,
+            'changeOrigin': true,
+            'pathRewrite': { '^/api' : '' },
+        },
+    },
+    define: {
+        'process.env': process.env
+    }
+});
+
