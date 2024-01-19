afc2351371407baf7c34eb6a790c679e46012e2d
SpectreAlan <comjszoo@gmail.com>
2024-01-19 12:42:22 +0800

    add global loading

---
+++ src/loading.tsx
@@ -0,0 +1,9 @@
+import '@/assets/style/loading.less'
+export default () => {
+    return (
+        <div className="global-loading-body">
+            <div className="loader">Loading...</div>
+        </div>
+    );
+};
+
