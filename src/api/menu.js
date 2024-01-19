8a5232dc0456b0e8e607e140202124b2629773fa
SpectreAlan <comjszoo@gmail.com>
2024-01-19 12:27:20 +0800

    init new project whit umi4

--- src/api/menu.js
+++
@@ -1,9 +0,0 @@
-import Request from '@/utils/request'
-
-export function menu(type, data) {
-  return Request.post('/menu/' + type, data)
-}
-
-export function types(data) {
-  return Request.post('/menu/search/types', data)
-}
