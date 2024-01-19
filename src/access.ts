58ea7473e49d3787306a66e94850161be0e53aa4
SpectreAlan <comjszoo@gmail.com>
2024-01-19 19:57:57 +0800

    add access

--- src/access.ts
+++ src/access.ts
@@ -1,10 +1,7 @@
 export default (initialState: API.UserInfo) => {
-  // 在这里按照初始化数据定义项目中的权限，统一管理
-  // 参考文档 https://umijs.org/docs/max/access
-  const canSeeAdmin = !!(
-    initialState && initialState.name !== 'dontHaveAccess'
-  );
   return {
-    canSeeAdmin,
+    canEdit: initialState.role === 'admin',
+    canDelete: initialState.role === 'admin',
+    canCreate: initialState.role === 'admin',
   };
 };
