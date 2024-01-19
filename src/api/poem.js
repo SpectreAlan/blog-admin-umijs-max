d054f144d0bcade4d07069a0cac848bf518920c8
SpectreAlan <comjszoo@gmail.com>
2024-01-19 12:27:20 +0800

    init new project whit umi4

--- src/api/poem.js
+++
@@ -1,9 +0,0 @@
-import Request from '@/utils/request'
-
-export function search(data) {
-  return Request.post('/poem/search', data)
-}
-
-export function del(data) {
-  return Request.post('/poem/del', data)
-}
--- src/views/admin/menu/config.js
+++
@@ -1,7 +0,0 @@
-export const types = ['按钮', '菜单', '目录']
-export function getSvg() {
-  const files = require.context('../../../icons/svg', false, /.(svg)$/)
-  for (let i = 0; i < files.keys().length; i++) {
-    this.svg.push(files.keys()[i].match(/\.\/(\S*)\.svg/)[1])
-  }
-}
