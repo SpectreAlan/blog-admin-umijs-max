062143de7e18cb7f331fa877ee12e7a24f08044f
SpectreAlan <comjszoo@gmail.com>
2024-01-20 20:04:12 +0800

    add user module

---
+++ src/pages/User/addOrEdit.tsx
@@ -0,0 +1,121 @@
+import {
+    DrawerForm,
+    ProFormText,
+    ProFormSwitch,
+    ProFormRadio
+} from '@ant-design/pro-components';
+import {Form} from 'antd';
+import {useEffect} from "react";
+import {useRequest} from "@umijs/max";
+import {CommonType} from "@/types/common.typings";
+
+const AddOrEdit: React.FC<CommonType.IProps> = ({setDrawerVisible, id, actionRef}) => {
+    const [form] = Form.useForm();
+
+    const {loading, run: queryRun} = useRequest(`/user/${id}`,
+        {
+            manual: true,
+            onSuccess: (res) => {
+                form.setFieldsValue(res)
+            }
+        });
+
+    const {loading: saveLoading, run: saveRun} = useRequest(
+        (data: User.UserItem) => {
+            if (id) {
+                return {
+                    method: 'PATCH',
+                    url: `/user/${id}`,
+                    data,
+                }
+            }
+            return {
+                method: 'POST',
+                url: `/user`,
+                data,
+            }
+        },
+        {
+            manual: true,
+            onSuccess: () => {
+                setDrawerVisible(false);
+                actionRef.current?.reloadAndRest?.();
+            }
+        });
+
+    useEffect(() => {
+        if (id) {
+            queryRun()
+        }
+    }, [])
+
+    return (
+        <DrawerForm<User.UserItem>
+            loading={loading || saveLoading}
+            onOpenChange={setDrawerVisible}
+            open={true}
+            title={id ? '编辑用户' : '新建用户'}
+            resize={{
+                onResize() {
+                    console.log('resize!');
+                },
+                maxWidth: window.innerWidth * 0.8,
+                minWidth: 300,
+            }}
+            form={form}
+            autoFocusFirstInput
+            drawerProps={{
+                destroyOnClose: true,
+            }}
+            submitTimeout={2000}
+            onFinish={async (values) => {
+                saveRun(values)
+            }}
+        >
+            <ProFormText
+                name="account"
+                width="md"
+                label="账号"
+                placeholder="请输入账号"
+                rules={[{required: true}]}
+            />
+            <ProFormText
+                name="nickName"
+                width="md"
+                label="昵称"
+                placeholder="请输入昵称"
+                rules={[{required: true}]}
+            />
+            <ProFormText
+                name="email"
+                width="md"
+                label="Email"
+                placeholder="请输入email"
+                rules={[{required: true}]}
+            />
+            <ProFormSwitch
+                name="status"
+                label="状态"
+                fieldProps={{checkedChildren: '启用', unCheckedChildren: '禁用'}}
+                rules={[{required: true}]}
+            />
+            <ProFormRadio.Group
+                name="role"
+                label="角色"
+                options={[
+                    {
+                        label: '管理员',
+                        value: 'admin',
+                    },
+                    {
+                        label: '普通用户',
+                        value: 'default',
+                    }
+                ]}
+                rules={[{required: true}]}
+            />
+        </DrawerForm>
+    );
+};
+
+export default AddOrEdit
--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -1,31 +1,113 @@
-import {ActionType, FooterToolbar, PageContainer,  ProTable} from '@ant-design/pro-components';
+import {ActionType, FooterToolbar, PageContainer, ProTable} from '@ant-design/pro-components';
 import {Access, useAccess, useRequest} from '@umijs/max';
-import {Button} from 'antd';
+import {Button, Avatar, Tag, Space, Modal} from 'antd';
 import React, {useRef, useState} from "react";
+import {SmileOutlined, FrownOutlined, ExclamationCircleFilled} from '@ant-design/icons';
+import AddOrEdit from "@/pages/User/addOrEdit";
 
 const UserPage: React.FC = () => {
     const access = useAccess();
     const actionRef = useRef<ActionType>();
+    const [selectedRows, setSelectedRows] = useState<User.UserItem[]>([]);
+
+    const [id, setId] = useState<string>('');
+    const [drawerVisible, setDrawerVisible] = useState(false)
+
+    const {loading: deleteLoading, run: batchDelete} = useRequest(
+        (ids: string[]) => {
+            return {
+                method: 'DELETE',
+                url: `/user`,
+                data: {ids},
+            }
+        },
+        {
+            manual: true,
+            onSuccess: () => {
+                setSelectedRows([]);
+                actionRef.current?.reloadAndRest?.();
+            }
+        });
+
+    const handleDelete = (ids: string[]) => {
+        Modal.confirm({
+            title: '温馨提示',
+            icon: <ExclamationCircleFilled/>,
+            content: `确定要删除所选项吗？`,
+            onOk() {
+                batchDelete(ids)
+            }
+        });
+    }
+    const handleEdit = (record: Poem.PoemItem) => {
+        setId(record.id)
+        setDrawerVisible(true)
+    }
+    const renderActions = (text: string, record: Poem.PoemItem) => (
+        <Space>
+            <Button type="link" onClick={() => handleEdit(record)}>
+                编辑
+            </Button>
+            <Button type="text" loading={deleteLoading} danger onClick={() => handleDelete([record.id])}>
+                删除
+            </Button>
+        </Space>
+    );
     const columns: any[] = [
         {
             title: '账号',
             dataIndex: 'account',
-            tip: '账号是唯一的 key',
-            formItemProps: {
-                rules: [
-                    {
-                        required: true,
-                        message: '账号为必填项',
-                    },
-                ],
+        },
+        {
+            title: '状态',
+            dataIndex: 'status',
+            valueEnum: {
+                1: {
+                    text: '正常',
+                    status: 'Success',
+                    icon: <SmileOutlined/>,
+                },
+                0: {
+                    text: '禁用',
+                    status: 'Error',
+                    icon: <FrownOutlined/>,
+                }
             },
+        },
+        {
+            title: '昵称',
+            dataIndex: 'nickName'
         },
         {
             title: 'Email',
             dataIndex: 'email'
-        }
+        },
+        {
+            title: '角色',
+            dataIndex: 'role',
+            render: (role: string) => role === 'admin' ? <Tag color="processing" bordered={false}>管理员</Tag> :
+                <Tag color="default" bordered={false}>普通用户</Tag>
+        },
+        {
+            title: '头像',
+            dataIndex: 'avatar',
+            render: (avatar: string, user: User.UserItem) => <Avatar src={avatar} alt={user.nickName} size={40}/>
+        },
+        {
+            title: '创建时间',
+            dataIndex: 'createdAt'
+        },
+        {
+            title: '更新时间',
+            dataIndex: 'updatedAt'
+        },
+        {
+            title: '操作',
+            valueType: 'option',
+            render: renderActions,
+        },
     ]
-    const [selectedRowsState, setSelectedRows] = useState<User.UserItem[]>([]);
+
     const {loading, run} = useRequest((params) => {
         return {
             method: 'GET',
@@ -37,6 +119,9 @@ const UserPage: React.FC = () => {
     });
     return (
         <PageContainer>
+            {
+                drawerVisible ? <AddOrEdit setDrawerVisible={setDrawerVisible} id={id} actionRef={actionRef}/> : null
+            }
             <ProTable<User.UserItem>
                 actionRef={actionRef}
                 loading={loading}
@@ -50,6 +135,8 @@ const UserPage: React.FC = () => {
                             key="add"
                             type="primary"
                             onClick={() => {
+                                setId('')
+                                setDrawerVisible(true)
                             }}
                         >
                             新建
@@ -69,12 +156,12 @@ const UserPage: React.FC = () => {
                     onChange: (_, selectedRows) => setSelectedRows(selectedRows),
                 }}
             />
-            {selectedRowsState?.length > 0 && (
+            {selectedRows?.length > 0 && (
                 <FooterToolbar
                     extra={
                         <div>
                             已选择{' '}
-                            <a style={{fontWeight: 600}}>{selectedRowsState.length}</a>{' '}
+                            <a style={{fontWeight: 600}}>{selectedRows.length}</a>{' '}
                             项&nbsp;&nbsp;
                         </div>
                     }
@@ -83,8 +170,7 @@ const UserPage: React.FC = () => {
                         <Button
                             type="primary" danger
                             onClick={async () => {
-                                setSelectedRows([]);
-                                actionRef.current?.reloadAndRest?.();
+                                handleDelete(selectedRows.map(item => item.id))
                             }}
                         >
                             批量删除
