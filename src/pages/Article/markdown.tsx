86ee55d5cae63d60c5735d59501bc862ba0cf026
SpectreAlan <comjszoo@gmail.com>
2024-01-23 14:08:57 +0800

    finished create article

--- src/pages/Article/markdown.tsx
+++ src/pages/Article/markdown.tsx
@@ -1,16 +1,17 @@
 import React, {useEffect, forwardRef, useRef} from "react";
+// @ts-ignore
 import Editor from '@toast-ui/editor';
 import '@toast-ui/editor/dist/toastui-editor.css';
 import '@toast-ui/editor/dist/theme/toastui-editor-dark.css';
 
-const MarkdownEditors = forwardRef(function MarkdownEditor({ content = '', setEditor }: any) {
+const MarkdownEditors = forwardRef(function MarkdownEditor({ setEditor }: any) {
     const editorRef = useRef(null)
     useEffect(() => {
         const editor = new Editor({
             el: editorRef.current,
             previewStyle: 'vertical',
             height: '500px',
-            initialValue: content,
+            initialValue: '',
             placeholder: 'Have a nice day!'
         });
         setEditor(editor)
