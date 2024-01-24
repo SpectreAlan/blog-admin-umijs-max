c82f46c93291fa57fa62be3985d639333d9f82d3
SpectreAlan <comjszoo@gmail.com>
2024-01-24 20:31:23 +0800

    Add comment report

---
+++ src/pages/Home/charts/comments.tsx
@@ -0,0 +1,40 @@
+import {useRequest} from '@umijs/max';
+import ReactECharts from 'echarts-for-react';
+import {IPropsStatistics} from '../utils'
+import {useEffect} from "react";
+
+const Comments: React.FC<IPropsStatistics> = (params) => {
+    const {data = {xAxis: [], series: []}, loading, run} = useRequest({
+        url: '/comment/statistics',
+        method: 'GET',
+        params
+    }, {manual: true});
+
+    useEffect(() => {
+        run()
+    }, [params])
+    const option = {
+        title: {
+            text: '评论统计'
+        },
+        tooltip: {},
+        xAxis: {
+            data: data.xAxis
+        },
+        yAxis: {},
+        series: [{
+            name: '评论数量',
+            type: 'bar',
+            data: data.series
+        }]
+    };
+    console.log(data);
+    return <ReactECharts
+        showLoading={loading}
+        option={option}
+        style={{height: 400}}
+        opts={{renderer: 'svg'}}
+    />
+};
+
+export default Comments;
